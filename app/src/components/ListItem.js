424d92f8a0ee6939d3c24e78b87f741f71b16583
rishikeshgaikar <rishikeshgaikar07@gmail.com>
2019-07-15 18:21:24 +0530

    created list item

--- app/src/components/ListItem.js
+++ app/src/components/ListItem.js
@@ -1,24 +1,25 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import StarRating from "./StarRating";
 
 const ListItem = ({ p_image, p_name, p_producer, p_cost, p_rating }) => (
-  <View style={{ flex: 1, flexDirection: "row" }}>
-    <View style={{ flex: 1 }}>
-      <Image source={{ uri: p_image }} style={{ height: 50, width: 50 }} />
-    </View>
-    <View style={{ flex: 4 }}>
-      <Text>{p_name}</Text>
-      <Text>{p_producer}</Text>
-      <View style={{ flex: 1, flexDirection: "row" }}>
-        <View style={{ flex: 4 }}>
-          <Text>Rs. {p_cost}</Text>
-        </View>
-        <View style={{ flex: 2 }}>
-          <Text>{p_rating}</Text>
+  <TouchableOpacity>
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1 }}>
+        <Image source={{ uri: p_image }} style={{ height: 50, width: 50 }} />
+      </View>
+      <View style={{ flex: 4 }}>
+        <Text>{p_name}</Text>
+        <Text>{p_producer}</Text>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <View style={{ flex: 4 }}>
+            <Text>Rs. {p_cost}</Text>
+          </View>
+          <StarRating count={p_rating} />
         </View>
       </View>
     </View>
-  </View>
+  </TouchableOpacity>
 );
 
 export default ListItem;
---
+++ app/src/components/StarRating.js
@@ -0,0 +1,142 @@
+import React, { Component } from "react";
+import { Image, StyleSheet, View, Text } from "react-native";
+import R from "../R";
+
+export default class StarRating extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    switch (this.props.count) {
+      case 1:
+        return (
+          <View style={{ flex: 2, flexDirection: "row" }}>
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_unchek}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_unchek}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_unchek}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_unchek}
+              style={{ height: 16, width: 16 }}
+            />
+          </View>
+        );
+      case 2:
+        return (
+          <View style={{ flex: 2, flexDirection: "row" }}>
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_unchek}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_unchek}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_unchek}
+              style={{ height: 16, width: 16 }}
+            />
+          </View>
+        );
+      case 3:
+        return (
+          <View style={{ flex: 2, flexDirection: "row" }}>
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_unchek}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_unchek}
+              style={{ height: 16, width: 16 }}
+            />
+          </View>
+        );
+      case 4:
+        return (
+          <View style={{ flex: 2, flexDirection: "row" }}>
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_unchek}
+              style={{ height: 16, width: 16 }}
+            />
+          </View>
+        );
+      case 5:
+        return (
+          <View style={{ flex: 2, flexDirection: "row" }}>
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+            <Image
+              source={R.images.star_check}
+              style={{ height: 16, width: 16 }}
+            />
+          </View>
+        );
+      default:
+        return <Text>Rating not available</Text>;
+    }
+  }
+}
+
+const styles = StyleSheet.create({});
