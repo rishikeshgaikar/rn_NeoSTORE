dfa903c122d35c0463a93cdbe7d308c37d5b0499
rishikeshgaikar <rishikeshgaikar07@gmail.com>
2019-07-25 10:49:48 +0530

    styled product listing

--- app/src/scenes/ProductList.js
+++ app/src/scenes/ProductList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Image, View, FlatList } from "react-native";
-import ListItem from "../components/ListItem";
+import { Image, View, FlatList, TouchableOpacity, Text } from "react-native";
+import StarRating from "../components/StarRating";
+import R from "../R";
 
 export default class ProductList extends Component {
   constructor() {
@@ -50,21 +51,50 @@ export default class ProductList extends Component {
         <FlatList
           data={this.state.dataSource}
           renderItem={({ item }) => (
-            <View>
-              <ListItem
-                onPress={() =>
-                  this.props.navigation.navigate("ItemDetails", {
-                    productID: item.id,
-                    productName: item.name
-                  })
-                }
-                p_image={item.product_images}
-                p_name={item.name}
-                p_producer={item.producer}
-                p_cost={item.cost}
-                p_rating={item.rating}
-              />
-            </View>
+            <TouchableOpacity
+              onPress={() =>
+                this.props.navigation.navigate("ItemDetails", {
+                  productID: item.id,
+                  productName: item.name
+                })
+              }
+            >
+              <View style={{ flex: 1, flexDirection: "row", margin: 10 }}>
+                <View style={{ flex: 1, paddingLeft: 10, paddingRight: 20 }}>
+                  <Image
+                    source={{ uri: item.product_images }}
+                    style={{ height: 70, width: 70 }}
+                  />
+                </View>
+                <View style={{ flex: 4 }}>
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      fontFamily: R.fonts.GothamBold
+                    }}
+                  >
+                    {item.name}
+                  </Text>
+
+                  <View
+                    style={{ flex: 1, flexDirection: "row", paddingTop: 10 }}
+                  >
+                    <View style={{ flex: 4 }}>
+                      <Text
+                        style={{
+                          color: R.colors.r2,
+                          fontSize: 20,
+                          fontFamily: R.fonts.GothamBold
+                        }}
+                      >
+                        Rs. {item.cost}
+                      </Text>
+                    </View>
+                    <StarRating count={item.rating} />
+                  </View>
+                </View>
+              </View>
+            </TouchableOpacity>
           )}
           keyExtractor={(item, index) => index.toString()}
         />
